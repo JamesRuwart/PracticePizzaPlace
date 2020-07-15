dcb2d018871eb3ed5f9cb465316402fe160d3b0f
jamesjr19 <ruwartja@gmail.com>
2020-07-15 15:38:30 -0400

    Added orders, orderItems, customer models and views

--- PracticePizzaPlace/Data/PizzaContext.cs
+++ PracticePizzaPlace/Data/PizzaContext.cs
@@ -12,8 +12,12 @@ namespace PracticePizzaPlace.Data
         public PizzaContext(DbContextOptions<PizzaContext> options) : base(options)
         {
         }
-            public DbSet<Employee> Employees { get; set; }
-        
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<OrderItemDetail> OrderItemDetails { get; set; }
 
     }
 }
---
+++ PracticePizzaPlace/Models/OrderItemDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticePizzaPlace.Models
+{
+    public enum OrderItemDetailType
+    {
+        Size,
+        Flavor,
+        Topping,
+        Sauce,
+        Cheese,
+        Crust
+
+    }
+
+    public class OrderItemDetail
+    {
+        public int ID { get; set; }
+        public OrderItemDetailType Type { get; set; }
+
+        //public InventoryItem Item { get; set; }
+
+    }
+}
